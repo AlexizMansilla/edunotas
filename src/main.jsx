d66e5f36c16c7918d7d559fb8ecb5288bece606e
AlexizMansilla <aggroman777@gmail.com>
2025-11-15 10:49:00 -0300

    cambion en import App from './App.jsx ' import SistemaNotasEscolar from './app.jsx' a import App from './app.jsx' import SistemaNotasEscolar from './app.jsx'

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx '
+import App from './app.jsx'
 import SistemaNotasEscolar from './app.jsx'
 
 
@@ -8,7 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <app />
         <SistemaNotasEscolar />
     </React.StrictMode>,
 )
