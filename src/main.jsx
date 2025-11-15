9b562ce10cdc33d8494087abfb5e9b0f94dc4c8d
AlexizMansilla <aggroman777@gmail.com>
2025-11-15 10:26:02 -0300

    Update main.jsx
    correcion en main.jsx: import App from './App.jsx '
    import SistemaNotasEscolar from './app.jsx'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx '
+import SistemaNotasEscolar from './app.jsx'
+
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <App />
         <SistemaNotasEscolar />
     </React.StrictMode>,
 )
