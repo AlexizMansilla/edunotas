ac3b9f0ad2c25ea8b6410c3851aafd00ae7f5f62
AlexizMansilla <aggroman777@gmail.com>
2025-11-15 12:05:15 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
-import SistemaNotasEscolar from './app.jsx'
+import SistemaNotasEscolar from './app.jsx' // Assuming this is also a top-level component, if not, consider removing or integrating into App
 
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <app />
+        {/* Component names must start with an uppercase letter */}
+        <App />
+        {/* If SistemaNotasEscolar is a separate component also to be rendered */}
         <SistemaNotasEscolar />
     </React.StrictMode>,
 )
