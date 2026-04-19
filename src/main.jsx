53d952b015d91b3ffb0c7efd686257591a446828
Alexis Mansilla <aggroman777@gmail.com>
2026-04-19 15:00:21 -0400

    Render SistemaNotasEscolar in StrictMode
    Fix: Remove duplicate component rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
-import SistemaNotasEscolar from './app.jsx' // Assuming this is also a top-level component, if not, consider removing or integrating into App
-
-
+import SistemaNotasEscolar from './app.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        {/* Component names must start with an uppercase letter */}
-        <App />
-        {/* If SistemaNotasEscolar is a separate component also to be rendered */}
-        <SistemaNotasEscolar />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <SistemaNotasEscolar />
+  </React.StrictMode>,
 )
