29d0b5a7aef3baa6e665848591a3c3c7924edc12
Alexis Mansilla <aggroman777@gmail.com>
2026-04-19 14:45:53 -0400

    Fix render issue in main.jsx by removing duplicate component rendering and fixing imports.

---
+++ main.jsx
@@ -0,0 +1,15 @@
+// Fixed imports and removed duplicate component rendering
+
+import React from 'react';
+import MyComponent from './MyComponent';
+// Add other necessary imports here
+
+const App = () => {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default App;
